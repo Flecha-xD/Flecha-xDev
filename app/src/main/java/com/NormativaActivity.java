852a85f974ae4453cbedd15b6500771816fe3990
fernanda <f.l.m.m.0027@gmail.com>
2020-11-30 11:25:38 -0300

    activity de contacto funcional y harcodeado de normativas

--- app/src/main/java/com/NormativaActivity.java
+++ app/src/main/java/com/NormativaActivity.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import androidx.cardview.widget.CardView;
 import android.view.View;
 import android.widget.GridLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.Adapters.NormativaAdapter;
+import com.model.Normativa;
+import java.util.LinkedList;
+import java.util.List;
 
 public class NormativaActivity extends AppCompatActivity {
 
@@ -16,6 +21,29 @@ public class NormativaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normativa);
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewMenu);
+
+        List<Normativa> normativaList = new LinkedList<>();
+        normativaList.add (new Normativa ("Ley N° 456","La presente " +
+                "Ley tiene por objeto regular los derechos y garantías de las usuarias y" +
+                " los usuarios, las consumidoras y los consumidores. "));
+        normativaList.add (new Normativa ("Reglamento de Regulación de Supermercados",
+                " El presente decreto supremo tiene por objeto la regulación del" +
+                        " establecimiento y funcionamiento de las bolsas de productos, los mercados " +
+                        "por ellas organizados, los sujetos que participen en los mismos, los actos y " +
+                        "contratos que se celebren, así como los productos negociables en dichos mercados, " +
+                        "de acuerdo con el Capítulo II del Título IV de la Ley del Mercado de Valores."));
+        normativaList.add (new Normativa ("Reglamento de Publicidad Engañosa y Abuso",
+                "El presente Reglamento tiene por objeto reglamentar el proceso de verificación y" +
+                        " restauración en protección a los Derechos a la Información y a la libre elección, " +
+                        "previniendo el incumplimiento del servicio ofertado y la utilización de la Publicidad " +
+                        "Engañosa o Abusiva de productos y servicios, así como la aplicación de sanciones administrativas. "));
+        NormativaAdapter adapter = new NormativaAdapter(this, normativaList);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
         menuGrid = (GridLayout) findViewById(R.id.menuGrid);
         setSingleEvent(menuGrid);
     }

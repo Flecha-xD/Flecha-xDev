2debdcfcac08db4faa929812e3c50bdd9a85d587
Sergio Laguna <sergiol@arcediazadrianoutlook.onmicrosoft.com>
2020-12-19 01:26:56 -0400

    eliminacion de reclamo completa, jalar info del recycler view, poner trua al boolean de atencion

--- app/src/main/java/proingsoftware/model/ReclamoFirebase.java
+++ app/src/main/java/proingsoftware/model/ReclamoFirebase.java
@@ -43,6 +43,14 @@ public class ReclamoFirebase {
         this.fueAtendido = fueAtendido;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getNombre() {
         return nombre;
     }
